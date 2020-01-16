1aba970b175320dd472212dba8459146e13e7ca8
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2020-01-17 00:28:47 +0300

    add linter, fix bugs

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-'use strict';
+'use strict'
 
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const router = express.Router();
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
 
 async function handleResponse (topicArn, req, res) {
   console.log('req.headers', req.headers)
@@ -11,53 +10,54 @@ async function handleResponse (topicArn, req, res) {
 }
 
 module.exports = () => {
-  const app = express();
+  const app = express()
+  const topicArnBounce = {}
+  const topicArnComplaint = {}
 
-  app.use(bodyParser.json());
+  app.use(bodyParser.json())
   app.use((req, res, next) => {
     // IMPORTANT, otherwise content-type is text for topic confirmation reponse, and body is emptys
     if (req.get('x-amz-sns-message-type')) {
-      req.headers['content-type'] = 'application/json';
+      req.headers['content-type'] = 'application/json'
     }
-    next();
-  });
+    next()
+  })
 
   app.post('/sns/handle-bounces', async (req, res, next) => {
     try {
-      await handleResponse(topicArnBounce, req, res);
+      await handleResponse(topicArnBounce, req, res)
 
       res.status(200).json({
         success: true,
         message: 'Successfully received message'
-      });
+      })
     } catch (err) {
-      next(err);
+      next(err)
     }
-  });
+  })
 
   app.post('/sns/handle-complaints', async function(req, res, next) {
     try {
-      handleResponse(topicArnComplaint, req, res);
+      handleResponse(topicArnComplaint, req, res)
 
       res.status(200).json({
         success: true,
         message: 'Successfully received message.'
-      });
-    } catch (error) {
-      next(err);
+      })
+    } catch (err) {
+      next(err)
     }
-  });
+  })
 
   app.use((err, req, res, next) => {
-    console.log(err);
+    console.log(err)
 
     res.json({
       ok: true
-    });
-  });
+    })
+  })
 
+  app.server = http.createServer(app)
 
-  app.server = http.createServer(app);
-
-  return app;
-};
+  return app
+}
